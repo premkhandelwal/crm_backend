da3d3d841cd3ed66a715efe870e309d221f569dd
premkhandelwal <prem.dkhandelwal@gmail.com>
2023-12-16 11:42:40 +0530

    Added edit and delete call for masters

--- src/controllers/master.controller.ts
+++ src/controllers/master.controller.ts
@@ -18,22 +18,50 @@ async function addCustomer(req: Request, res: Response) {
   return res.json(savedCustomer)
 }
 
-async function isUserExist(req: Request, res: Response) {
-  const getgoogleId = req.body.googleId
-  console.log(getgoogleId)
-  const customerData: Model<any> = await Customer.findOne({
-    googleId: getgoogleId,
-  }).catch((error) => {
-    console.log(error)
-    res.status(400).json(error)
-  })
-  console.log(customerData)
-  const data = JSON.parse(JSON.stringify(customerData))
-  if (data != null) {
-    data.id = data._id
+async function editCustomer(req: Request, res: Response) {
+  const customerId = req.body.id;
+
+  try {
+    if (!customerId) {
+      return res.status(400).json({ error: 'Invalid customer ID provided' });
+    }
+
+    const updatedCustomer = await Customer.findByIdAndUpdate(
+      customerId,
+      req.body,
+
+    );
+
+    if (!updatedCustomer) {
+      return res.status(404).json({ error: 'Customer not found' });
+    }
+
+    return res.status(200).json(updatedCustomer);
+  } catch (error) {
+    console.error('Error editing Customer:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
-  console.log(data)
-  return res.status(200).json(data)
+}
+
+async function deleteCustomer(req: Request, res: Response) {
+  const customerId = req.body.id;
+
+  try {
+    if (!customerId) {
+      return res.status(400).json({ error: 'Invalid customer ID provided' });
+    }
+
+    const deletedCustomer = await Customer.findByIdAndDelete(customerId);
+
+    if (!deletedCustomer) {
+      return res.status(404).json({ error: 'Customer not found' });
+    }
+
+    return res.status(200).json(deletedCustomer);
+  } catch (error) {
+    console.error('Error deleting Customer:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
 
 async function getAllUsers(req: Request, res: Response) {
@@ -51,6 +79,8 @@ async function getAllUsers(req: Request, res: Response) {
     return res.status(500).json(error)
   }
 }
+
+
 
 async function addMake(req: Request, res: Response) {
   const makeData = {
@@ -77,6 +107,52 @@ async function getAllMake(req: Request, res: Response) {
   }
 }
 
+async function editMake(req: Request, res: Response) {
+  const makeId = req.body.id;
+
+  try {
+
+    if (!makeId) {
+      return res.status(400).json({ error: 'Invalid make ID provided' });
+    }
+    const updatedMake = await Make.findByIdAndUpdate(
+      makeId,
+      req.body
+    );
+
+    if (!updatedMake) {
+      return res.status(404).json({ error: 'Make not found' });
+    }
+    return res.status(200).json(updatedMake);
+  } catch (error) {
+    console.error('Error editing Make:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+async function deleteMake(req: Request, res: Response) {
+  const makeId = req.body.id;
+
+  try {
+
+    if (!makeId) {
+      return res.status(400).json({ error: 'Invalid make ID provided' });
+    }
+    const deletedMake = await Make.findByIdAndDelete(
+      makeId
+    );
+
+    if (!deletedMake) {
+      return res.status(404).json({ error: 'Make not found' });
+    }
+    return res.status(200).json(deletedMake);
+  } catch (error) {
+    console.error('Error deleting Make:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+
 async function addHarness(req: Request, res: Response) {
   const harnessData = {
     name: req.body.name,
@@ -85,6 +161,55 @@ async function addHarness(req: Request, res: Response) {
   const savedHarness = await harness.save()
   return res.json(savedHarness);
 }
+
+async function editHarness(req: Request, res: Response) {
+  const harnessId = req.body.id;
+
+  try {
+    if (!harnessId) {
+      return res.status(400).json({ error: 'Invalid harness ID provided' });
+    }
+
+    const updatedHarness = await Harness.findByIdAndUpdate(
+      harnessId,
+      req.body,
+
+    );
+
+    if (!updatedHarness) {
+      return res.status(404).json({ error: 'Harness not found' });
+    }
+
+    return res.status(200).json(updatedHarness);
+  } catch (error) {
+    console.error('Error editing Harness:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+
+async function deleteHarness(req: Request, res: Response) {
+  const harnessId = req.body.id;
+
+  try {
+    if (!harnessId) {
+      return res.status(400).json({ error: 'Invalid harness ID provided' });
+    }
+
+    const deletedHarness = await Harness.findByIdAndDelete(harnessId);
+
+    if (!deletedHarness) {
+      return res.status(404).json({ error: 'Harness not found' });
+    }
+
+    return res.status(200).json(deletedHarness);
+  } catch (error) {
+    console.error('Error deleting Harness:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+
 
 async function getAllHarness(req: Request, res: Response) {
   try {
@@ -129,8 +254,52 @@ async function getAllBms(req: Request, res: Response) {
 }
 
 
+async function editBms(req: Request, res: Response) {
+  const bmsId = req.body.id;
+
+  try {
+    if (!bmsId) {
+      return res.status(400).json({ error: 'Invalid Bms ID provided' });
+    }
+
+    const updatedBms = await Bms.findByIdAndUpdate(
+      bmsId,
+      req.body,
+
+    );
+
+    if (!updatedBms) {
+      return res.status(404).json({ error: 'Bms not found' });
+    }
+
+    return res.status(200).json(updatedBms);
+  } catch (error) {
+    console.error('Error editing Bms:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
 
 
+async function deleteBms(req: Request, res: Response) {
+  const bmsId = req.body.id;
+
+  try {
+    if (!bmsId) {
+      return res.status(400).json({ error: 'Invalid Bms ID provided' });
+    }
+
+    const deletedBms = await Bms.findByIdAndDelete(bmsId);
+
+    if (!deletedBms) {
+      return res.status(404).json({ error: 'Bms not found' });
+    }
+
+    return res.status(200).json(deletedBms);
+  } catch (error) {
+    console.error('Error deleting Bms:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
 
 async function addBatch(req: Request, res: Response) {
   try {
@@ -149,8 +318,56 @@ async function addBatch(req: Request, res: Response) {
     console.error(error)
     return res.status(500).json(error)
   }
- 
+
 }
+
+
+async function editBatch(req: Request, res: Response) {
+  const batchId = req.body.id;
+
+  try {
+    if (!batchId) {
+      return res.status(400).json({ error: 'Invalid batch ID provided' });
+    }
+
+    const updatedBatch = await Batch.findByIdAndUpdate(
+      batchId,
+      req.body,
+    );
+
+    if (!updatedBatch) {
+      return res.status(404).json({ error: 'Batch not found' });
+    }
+
+    return res.status(200).json(updatedBatch);
+  } catch (error) {
+    console.error('Error editing Batch:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+
+async function deleteBatch(req: Request, res: Response) {
+  const batchId = req.body.id;
+
+  try {
+    if (!batchId) {
+      return res.status(400).json({ error: 'Invalid batch ID provided' });
+    }
+
+    const deletedBatch = await Batch.findByIdAndDelete(batchId);
+
+    if (!deletedBatch) {
+      return res.status(404).json({ error: 'Batch not found' });
+    }
+
+    return res.status(200).json(deletedBatch);
+  } catch (error) {
+    console.error('Error deleting Batch:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
 
 
 async function getBatch(req: Request, res: Response) {
@@ -191,15 +408,24 @@ async function getAllBatch(req: Request, res: Response) {
 
 export const masters = {
   addUser: addCustomer,
-  isUserExist: isUserExist,
+  editCustomer: editCustomer,
+  deleteCustomer: deleteCustomer,
   getAllUsers: getAllUsers,
   addBatch: addBatch,
+  editBatch: editBatch,
+  deleteBatch: deleteBatch,
   getBatch: getBatch,
   getAllBatch: getAllBatch,
   addMake: addMake,
+  editMake: editMake,
+  deleteMake: deleteMake,
   getAllMake: getAllMake,
   addHarness: addHarness,
+  editHarness: editHarness,
+  deleteHarness: deleteHarness,
   getAllHarness: getAllHarness,
   addBms: addBms,
+  editBms: editBms,
+  deleteBms: deleteBms,
   getAllBms: getAllBms,
 }
--- src/routes/master.routes.ts
+++ src/routes/master.routes.ts
@@ -3,25 +3,30 @@ import { masters } from '../controllers/master.controller'
 
 const router: Router = Router()
 
-router.post('/isUserExist', masters.isUserExist)
-
 router.post('/addCustomer', masters.addUser)
+router.put('/editCustomer', masters.editCustomer)
+router.delete('/deleteCustomer', masters.deleteCustomer)
 router.get('/getAllCustomers', masters.getAllUsers)
 
 router.post('/addBatch', masters.addBatch)
+router.put('/editBatch', masters.editBatch)
+router.delete('/deleteBatch', masters.deleteBatch)
 router.get('/getBatch', masters.getBatch)
 router.get('/getAllBatch', masters.getAllBatch)
 
-router.post('/addCustomer', masters.addUser)
-router.get('/getAllCustomers', masters.getAllUsers)
-
 router.post('/addMake', masters.addMake)
+router.put('/editMake', masters.editMake)
+router.delete('/deleteMake', masters.deleteMake)
 router.get('/getAllMake', masters.getAllMake)
 
 router.post('/addHarness', masters.addHarness)
+router.put('/editHarness', masters.editHarness)
+router.delete('/deleteHarness', masters.deleteHarness)
 router.get('/getAllHarness', masters.getAllHarness)
 
 router.post('/addBms', masters.addBms)
+router.put('/editBms', masters.editBms)
+router.delete('/deleteBms', masters.deleteBms)
 router.get('/getAllBms', masters.getAllBms)
 
 
